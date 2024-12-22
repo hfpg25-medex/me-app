96fb5e953439391e11683ffeb8a2f7c523619591
Du Yanchao <du.yanchao@gt.tech.gov.sg>
2024-12-22 19:35:47 +0800

    Add clinicDetailsAction

--- lib/actions.ts
+++ lib/actions.ts
@@ -148,3 +148,50 @@ export async function doctorAction(
   }
 }
 
+export async function clinicDetailsAction(
+  _prevState: unknown,
+  formData: FormData
+) {
+  const defaultValues = z
+    .record(z.string(), z.string())
+    .parse(Object.fromEntries(formData.entries()))
+
+  try {
+    const data = clinicSchema.parse(Object.fromEntries(formData))
+
+    // This simulates a slow response like a form submission.
+    // Replace this with your actual form submission logic.
+    await new Promise(resolve => setTimeout(resolve, 1000))
+
+    console.log(data)
+
+    return {
+      defaultValues: {
+        hcCode: '',
+        contactNumber: '',
+      },
+      success: true,
+      errors: null,
+    }
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return {
+        defaultValues,
+        success: false,
+        errors: Object.fromEntries(
+          Object.entries(error.flatten().fieldErrors).map(([key, value]) => [
+            key,
+            value?.join(', '),
+          ])
+        ),
+      }
+    }
+
+    return {
+      defaultValues,
+      success: false,
+      errors: null,
+    }
+  }
+}
+
