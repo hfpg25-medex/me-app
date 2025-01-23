5a52f32d2280a767fda841d2f7fc6a948bb573d1
Du Yanchao <du_yanchao@tech.gov.sg>
2025-01-23 18:33:14 +0800

    Udpate exam title and link

--- app/medical-exam/select/page.tsx
+++ app/medical-exam/select/page.tsx
@@ -17,9 +17,9 @@ export default function SelectExamPage() {
       href: "/medical-exam/mdw"
     },
     {
-      title: "Medical Exam for Work Permit Application",
+      title: "Medical Exam for Work Permit Issuance",
       description: "MOM",
-      href: "/medical-exam/wp"
+      href: "/medical-exam/wp-issue"
     },
     {
       title: "Six-monthly Medical Exam for Female Migrant Workers",
