441c9115b5001dc59ef5d1b9e26a098ed3299dab
Du Yanchao <du.yanchao@gt.tech.gov.sg>
2025-01-27 00:19:32 +0800

    #1 move exam titles to constants

---
+++ constants/exam-titles.ts
@@ -0,0 +1,6 @@
+export const examTitles = {
+  'fme': 'Full Medical Exam for Foreign Workers (MOM)',
+  'fmw': 'Six-monthly Medical Exam for Female Migrant Workers (MOM)',
+  'mdw': 'Six-monthly Medical Exam for Migrant Domestic Workers (MOM)',
+  'pr': 'Medical Exam for PR Application (ICA)',
+}
