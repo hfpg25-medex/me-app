e81b46c5d4af894ebea800c9d42a21261c2b17c0
Du Yanchao <du.yanchao@gt.tech.gov.sg>
2025-02-08 15:56:38 +0800

    chore:refactor as const

---
+++ constants/clinics.ts
@@ -0,0 +1,9 @@
+export const clinics = [
+  {
+    id: "1",
+    name: "ABC Medical Clinic (Bukit Batok)",
+    hciCode: "2M12345",
+    contactNumber: "+65 69991234",
+  },
+  // { id: '2', name: 'Healthline 24Hr Clinic (Jurong East)', hciCode: '2M54321', contactNumber: '+65 69995678' },
+];
---
+++ constants/doctors.ts
@@ -0,0 +1,4 @@
+export const doctors = [
+  { id: "1", name: "Dr. John Doe", mcrNumber: "M12345A" },
+  { id: "2", name: "Dr. Sarah Chen", mcrNumber: "M67890B" },
+];
